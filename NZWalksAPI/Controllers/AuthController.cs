4afe43ef19aa55f085aaaebe91494c5dbc88b191
Ankit Pandey <ankit@omaemirates.com>
2024-11-26 17:54:25 +0400

    User Registeration Part Added till this commit

---
+++ NZWalksAPI/Controllers/AuthController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        /*User Registeration Part Added till this commit */
+        //POST : /api/Auth/Register
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                PasswordHash = registerRequestDto.Password
+            };
+            var identityResult = await _userManager.CreateAsync(identityUser, registerRequestDto.Password);
+            if(identityResult.Succeeded)
+            {
+                //Add Role for user
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await _userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User Was Gerenated");
+                    }
+                }
+            }
+            return BadRequest("Something went wrong");
+        }
+    }
+}
---
+++ NZWalksAPI/DTO/RegisterRequestDTO.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using System.ComponentModel.DataAnnotations;
+
+namespace NZWalksAPI.DTO
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
