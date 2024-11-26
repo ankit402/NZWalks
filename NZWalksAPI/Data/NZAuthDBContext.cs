d64dedfa6cf0794ad51fdd756f672dc41e6074d9
Ankit Pandey <ankit@omaemirates.com>
2024-11-26 17:31:10 +0400

    updated and seek table for Roles

---
+++ NZWalksAPI/Data/NZAuthDBContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalksAPI.Data
+{
+    public class NZAuthDBContext  : IdentityDbContext
+    {
+        public NZAuthDBContext(DbContextOptions<NZAuthDBContext> dbcontext ) : base( dbcontext )
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var readerRoleId = "a72f5152-5cb7-47db-9e13-c8a9ff536029";
+            var WriterRoleId = "a82e5172-5cb8-47eb-9d13-c0532af536029";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp=readerRoleId,
+                    Name= "Reader",
+                    NormalizedName = "READER"
+                },
+                 new IdentityRole
+                {
+                    Id = WriterRoleId,
+                    ConcurrencyStamp=WriterRoleId,
+                    Name= "Writer",
+                    NormalizedName = "WRITER"
+                }
+            };
+
+            modelBuilder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
--- NZWalksAPI/Program.cs
+++ NZWalksAPI/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using NZWalksAPI.Data;
 using NZWalksAPI.Mapping;
 using NZWalksAPI.Repositories;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +17,38 @@ builder.Services.AddSwaggerGen();
 // Configure Entity Framework Core
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationConnectionString")));
+builder.Services.AddDbContext<NZAuthDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationConnectionString")));
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
-builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();  
-var app = builder.Build();
 
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("NZWalks")
+    .AddEntityFrameworkStores<NZAuthDBContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+{
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidateLifetime = true,
+    ValidateIssuerSigningKey = true,
+    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+    ValidAudience = builder.Configuration["Jwt:Audience"],
+    IssuerSigningKey =  new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+});
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -24,6 +56,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
